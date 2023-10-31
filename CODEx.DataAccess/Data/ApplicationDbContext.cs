c0f05bb30c58b2d99bfdaa4b4b85f1c920fa9999
$HIVAM <supshiv7250@gmail.com>
2023-11-01 00:56:25 +0530

    Added Registration Form

--- CODEx.DataAccess/Data/ApplicationDbContext.cs
+++ CODEx.DataAccess/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace CODEx.DataAccess.Data
 
         public DbSet<Coordinator> Coordinator { get; set; } 
 
+        public DbSet<RegistrationForm> RegistrationForm { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ CODEx.Model/RegistrationForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODEx.Model
+{
+    public class RegistrationForm
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? TeamName { get; set; }
+
+        public string? College {  get; set; }
+
+        public string? Department { get; set; }
+
+        public string? TeamLeaderNanme { get; set; }
+
+        public string? LeaderNumber  { get; set; }
+
+        public string? LeaderEmail { get; set; }
+
+        public string? Member2Name { get; set; }
+
+        public string? Member2Number { get; set; }
+
+        public string? Member3Name { get; set; }
+
+        public string? Member3Number { get; set; }
+
+        public string? Member4Name { get;set; }
+
+        public string? Member4Number { get;set; }
+
+        public string? Theme { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? ProblemStatment { get; set; }
+    }
+}
