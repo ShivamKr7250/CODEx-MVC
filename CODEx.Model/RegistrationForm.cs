5510d9ad242eb970b9510bd9029dd89e435e3309
$HIVAM <supshiv7250@gmail.com>
2023-11-01 10:22:23 +0530

    Updating the Registration Form Detail

--- CODEx.Model/RegistrationForm.cs
+++ CODEx.Model/RegistrationForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -18,28 +19,38 @@ namespace CODEx.Model
 
         public string? Department { get; set; }
 
-        public string? TeamLeaderNanme { get; set; }
+        [DisplayName("Leader Name")]
+        public string? TeamLeaderName { get; set; }
 
+        [DisplayName("Leader Phone Number")]
         public string? LeaderNumber  { get; set; }
 
+        [DisplayName("Leader EmailID")]
         public string? LeaderEmail { get; set; }
 
+        [DisplayName("Member 2 Name")]
         public string? Member2Name { get; set; }
 
+        [DisplayName("Member 2 Number")]
         public string? Member2Number { get; set; }
 
+        [DisplayName("Member 3 Name")]
         public string? Member3Name { get; set; }
 
+        [DisplayName("Member 3 Number")]
         public string? Member3Number { get; set; }
 
+        [DisplayName("Member 4 Name")]
         public string? Member4Name { get;set; }
 
+        [DisplayName("Member 4 Number")]
         public string? Member4Number { get;set; }
 
         public string? Theme { get; set; }
 
         public string? Category { get; set; }
 
+        [DisplayName("Problem Statement")]
         public string? ProblemStatment { get; set; }
     }
 }
